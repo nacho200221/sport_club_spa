0614ec176b8ded47739c2a137f018ae2a7040053
nacho200221 <ignacio.velasquez15@inacapmail.cl>
2026-07-09 13:08:34 -0400

    API localhost

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 // src/services/api.js
 
 // Si estamos en AWS usará la variable de entorno, si estamos en tu PC usará localhost
-export const API_URL = "http://32.193.216.0:3000/api";
+export const API_URL = "http://localhost:3000/api";
 export function getHeaders() {
   const token = localStorage.getItem("token");
   return {
